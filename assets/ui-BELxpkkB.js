03988a4e70c881db8167f410588ed73e11ce524c
sungchi <sungchi9@gmail.com>
2025-07-29 22:33:10 +0900

    mcp 모드 추가

--- assets/ui-BELxpkkB.js
+++ assets/ui-BELxpkkB.js
@@ -1,4 +1,4 @@
-import{r as n}from"./vendor-CnSK6Mz1.js";/**
+import{r as n}from"./vendor-CJtRYghK.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
